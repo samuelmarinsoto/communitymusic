4b8d5d3b2a5994f6d686b0dea7430ec3e7d29dbd
JMLoria <joloria@estudiantec.cr>
2024-03-05 22:20:05 -0600

    Revirtiendo algunos cambios

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 #include <stdio.h>
-#include "Lib/cpptk/cpptk.h"
+#include <SFML/Graphics.hpp>
 
-void hello(){
-    puts("Hello C++/Tk!");
-}
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-int main(int, char *argv[]){
-    init(argv[0]);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-    button(".b") -text("Say Hello") -command(hello);
-    pack(".b") -padx(20) - pady(6);
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
-    runEventLoop();
+    return 0;
 }
