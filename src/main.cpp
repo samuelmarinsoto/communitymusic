151fccf3086c4e8aa8736e349ac105e21f81f8c1
JMLoria <joloria@estudiantec.cr>
2024-03-05 19:26:50 -0600

    Eliminando carpeta cpptk

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,7 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include "Libs/cpptk/cpptk.h"
 
-using namespace Tk;
-using namespace std;
+int main(){
 
-int main(int, char *argv[]){
-    try{
-        init(argv[0]);
-        label(".l") -text("Hello C++/Tk!");
-        pack(".l") -padx(20) -pady(6);
-        
-        runEventLoop();
-    }
-    catch (exception const &e)
-    {
-        cerr << "Error: " << e.what() << '\n';
-    }
+
+    return 0;
 }
