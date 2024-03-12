ce4d25e2e3650a12c34fab1d6853792912ad6237
JMLoria <joloria@estudiantec.cr>
2024-03-11 19:12:37 -0600

    Set Tk

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,6 @@
 #include <iostream>
-#include <stdio.h>
-// #include "Lib/cpptk/cpptk.h"
-#include <SFML/Graphics.hpp>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
+int main(){
     return 0;
-}
-
-/*
-void hello(){
-    puts("Hello C++/Tk!");
-}
-
-int main(int, char *argv[]){
-    init(argv[0]);
-
-    button(".b") -text("Say Hello") -command(hello);
-    pack(".b") -padx(20) - pady(6);
-
-    runEventLoop();
-}
-*/
+};
