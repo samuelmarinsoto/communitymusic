14674f7f7afed8233035a72e808c0d0627e1ce6b
JMLoria <joloria@estudiantec.cr>
2024-03-11 19:21:47 -0600

    Estableciendo Tk

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <tk.h>
+
+using namespace Tk; 
 
 
-int main(){
-    return 0;
-};
+void hello() {
+    puts("Hello C++/Tk!");
+}
+
+int main(int, char *argv[])
+{
+    init(argv[0]);
+
+    button(".b") -text("Say Hello") -command(hello);
+    pack(".b") -padx(20) -pady(6);
+
+    runEventLoop();
+}
