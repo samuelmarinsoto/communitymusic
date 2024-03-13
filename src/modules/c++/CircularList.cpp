81af8810e102b3538c77cb5a39e48744a58f8a72
emarojass05 <e84671818@gmail.com>
2024-03-13 00:04:51 -0600

    lista doble circular terminada

--- src/modules/c++/CircularList.cpp
+++ src/modules/c++/CircularList.cpp
@@ -5,10 +5,12 @@ using namespace :: std;
 struct nodo {
     int data;
     Node* next;
+    Node* prev;
 }*first, *last;
 
 void insertSong();
-
+void ShowSongs();
+void ShowSongsBackwards();
 int main(){
     return 0;
 }
@@ -19,21 +21,43 @@ void insertSong(){
     cin >> new->data;
     if(first==NULL){
         first=new;
-        first->next=first;
         last=first;
+        first->next=first;
+        first->prev=last;
     }else{
         last->next=new;
+        new->prev=last;        
         new->next=first;
         last=new;
+        first->prev=last;
     }
 }
-
 void ShowSongs(){
-    Node* temp=first;
-    do{
-        cout << temp->data << " ";
-        temp=temp->next;
-    }while(temp!=first);
+    Node* temp=new Node();
+    temp=first;
+    if(first!=NULL){
+        do{
+            cout << temp->data << " ";
+            temp=temp->next;
+        }while(temp!=first);
+            cout << endl;
+    }else{
+        cout << "The list is empty";
+    }
     cout << endl;
 }
-```
+void ShowSongsBackwards();
+    Node* temp=new Node();
+    temp=last;
+    if(first!=NULL){
+        do{
+            cout << temp->data << " ";
+            temp=temp->prev;
+        }while(temp!=last);
+            cout << endl;
+    }else{
+        cout << "The list is empty";
+    }
+    cout << endl;
+}
+``` 
