bfa34614b3e1986de0a04b33ad39928c2469a606
fredeos <fobando@estudiantec.cr>
2024-03-21 14:26:53 -0600

    SFML for gui and sound
    Added main code for basic gui. Other modules of repo still inactive

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,35 @@
-#include "modules/c++/Server.hpp"
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
 
 #include <iostream>
 #include <string>
 
 
 int main(int argc, char *argv[]){
-    Server socket(49001 ,"127.0.0.1");
+    // Codigo de sever
+    /* Server socket(49001 ,"127.0.0.1");
     while (true) {
         string* x = socket.access_event();
         cout << x[0] << " said " << x[1] << endl;
+    } */
+    // Codigo de interfaz
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
     }
     return 0;
 }
