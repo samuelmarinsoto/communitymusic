f72bf5c247c3989d372038915db75edc24239d7a
emarojass05 <e84671818@gmail.com>
2024-03-25 00:27:41 -0600

    Commit prueba 1 InterfazSFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
-// #include "SFML/Graphics.hpp"
-// #include "SFML/Window.hpp"
-#include "lib/Vector.hpp"
-#include "lib/fJSON.hpp"
-#include "modules/c++/LinkedList.hpp"
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+// #include "lib/Vector.hpp"
+// #include "lib/fJSON.hpp"
+// #include "modules/c++/LinkedList.hpp"
+#include "SFML/Graphics.hpp" // Include statement for sf::Font
 
 #include <iostream>
 #include <string>
@@ -18,13 +19,14 @@ int main(int argc, char *argv[]){
     } */
 
     // Codigo de interfaz
-    /*sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(200, 200), "Comunity Playlist");
+    sf::Font font;
+        if (!font.loadFromFile("/home/ema/Downloads/Courier_Prime/CourierPrime-Bold.ttf")) {
+        
+    }
 
-    while (window.isOpen())
-    {
+
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -32,30 +34,59 @@ int main(int argc, char *argv[]){
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        window.clear(sf::Color::White); // Set background color to white
+
         window.display();
-    }*/
-    Dictionary json = JSON::parse("{\"cmd\":\"print\",\"attch\":[4,false,null],\"extra\":{\"key\":67,\"status\":\"ok\"}}");
-        json.add("extra", JSON::convert_to_value<string>("jhbjbdjkabd"));
-    std::cout << json["extra"].content << std::endl;
-
-    LinkedList<int> list;
-        list.insert(41);
-        list.insert(90);
-        list.insert(0);
-        list.insert(7);
-        list.find_remove(0);
-        list.pop();
-        list[1] = 2;
-    std::cout << "Node data: " << list[1] << "\nList size: " << list.size << std::endl;
 
 
-    vect<tuple <string, int> > arr;
-        arr.push(make_tuple("fff", 47));
-    //Array dict;
-        //dict.append(JSON::convert_to_value<int>(76));
-    //Value val = JSON::convert_to_value<Array>(dict);
-    std::cout << get<1> (arr[0]) << std::endl;
-    return 0;
-}
+    sf::Text text;
+    text.setFont(font);
+    text.setString("SoundScore");
+    text.setCharacterSize(22);
+    text.setFillColor(sf::Color::Black);
+    text.setStyle(sf::Text::Bold);
+    text.setPosition(window.getSize().x / 2 - text.getGlobalBounds().width / 2, 20.f); // Centered the text at the top
+
+    sf::RectangleShape button;
+    button.setSize(sf::Vector2f(60.f, 25.f)); // Made the button smaller
+    button.setFillColor(sf::Color::Blue); // Button color set to Blue
+    button.setPosition(window.getSize().x / 2 - button.getSize().x / 2, window.getSize().y / 2 + button.getSize().y); // Positioned the button lower
+
+            while (window.isOpen())
+            {
+                sf::Event event;
+                while (window.pollEvent(event))
+                {
+                    if (event.type == sf::Event::Closed)
+                        window.close();
+
+                    if (event.type == sf::Event::MouseButtonPressed) {
+                        if (event.mouseButton.button == sf::Mouse::Left) {
+                            sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+                            sf::Vector2f mousePosF(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
+                            if (button.getGlobalBounds().contains(mousePosF)) {
+                                sf::RenderWindow newWindow(sf::VideoMode(800, 600), "Comunity Playlist");
+                                while (newWindow.isOpen())
+                                {
+                                    sf::Event newEvent;
+                                    while (newWindow.pollEvent(newEvent))
+                                    {
+                                        if (newEvent.type == sf::Event::Closed)
+                                            newWindow.close();
+                                    }
+
+                                    newWindow.clear(sf::Color::White); // Set background color to white
+                                    newWindow.display();
+                                }
+                            }
+                        }
+                    }
+                }
+
+                window.clear(sf::Color::White); // Set background color to white
+                window.draw(button);
+                window.draw(text);
+                window.display();
+            }
+            return 0;
+        }
