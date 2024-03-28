241ac1340c7bcfba8d7a9a8eb431f41d8f93a321
fredeos <fobando@estudiantec.cr>
2024-03-28 13:59:22 -0600

    CircularList forward and backwards movements to iterate over songs

--- src/modules/c++/CircularList.h
+++ src/modules/c++/CircularList.h
@@ -60,12 +60,84 @@ class CircularList<MP3Tags> : public Observer{
     private:
         DoubleLinkedList<MP3Tags>* origin;
         string identifier;
-        Node<MP3Tags>* playingNow;  // Has a two-way link to the previous song() and the next song()
-                                    // Only this node is not a reference to the origin node, but a copy of the data, in case the origin list changes
         LinkedList<string> alreadyPlayed; // List of songs(uuids) that the have already been played and wont be played again(if the origin list changes), unless it is forced
         string start;  // Reference to the id of the song that was played first, and by consequence should play again when the playlist ends
+
+        // Changes the node order on the pointed origin list
+        void updateOrder(){
+            if (!this->conditioned){ // When the mp3 reproduction hasn't started
+                Node<MP3Tags>* ref = this->origin->GetNode(0); // A reference to the first node of the DoubleLinkedList
+                // Before changing any values must deallocate the pointers from memory
+                if (this->playingNow != nullptr){
+                    delete this->playingNow->next;
+                    delete this->playingNow->prev;
+                }
+                delete this->playingNow;
+                this->playingNow = nullptr;
+                // Now its viable to change any data
+                if (ref != nullptr){
+                    this->playingNow = new Node<MP3Tags>(ref->data); // Create a copy node of the contents
+                    if (ref->next != nullptr){
+                        this->playingNow->next = new Node<MP3Tags>(ref->next->data);
+                        this->playingNow->next->prev = this->playingNow;
+                    }
+                }
+                this->start = string(this->playingNow->data.uuid);
+            } else { // Once started
+                this->setFollowing();
+            }
+        }
+        // Sets a the node with better voting difference as the following node
+        void setFollowing(){
+            // Get the head node and iterate over the nodes to find the one with a better voting difference
+            // Set the better one AS the following node
+            Node<MP3Tags>* ref = this->origin->getHead();
+            Node<MP3Tags>* selected = nullptr;
+            int best_difference = 0;
+            while(ref != nullptr){
+                if ( (ref->data.upvotes - ref->data.downvotes) >= best_difference && ref != this->playingNow && this->check_played(string(ref->data.uuid)) ){
+                    selected = ref;
+                }
+                ref = ref->next;
+            }
+            if (selected != nullptr){
+                delete this->playingNow->next;
+                this->playingNow->next = nullptr;
+                
+                this->playingNow->next = new Node<MP3Tags>(selected->data);
+                this->playingNow->next->prev = this->playingNow;
+            }
+        }
+        // Sets a node marked as previously played as the
+        void setPrevious(){
+            Node<MP3Tags>* ref = this->origin->getHead();
+            while (ref != nullptr){
+                if(string(ref->data.uuid) == this->alreadyPlayed.get(this->alreadyPlayed.size -1)){
+                    this->alreadyPlayed.pop();
+                    this->playingNow->prev = new Node<MP3Tags>(ref->data);
+                    this->playingNow->prev->next = this->playingNow;
+                }
+                ref = ref->next;
+            }
+        }
+        // Checks if a song has already been played or not
+        // Compares their unique uuids
+        // Returns FALSE if not found, TRUE if found
+        bool check_played(string uuid){
+            Node<MP3Tags>* ref = this->origin->getHead();
+            while (ref != nullptr){
+                if (string(ref->data.uuid) == uuid){
+                    return true;
+                }
+                ref = ref->next;
+            }
+            return false;
+        }
     public:
-        bool ongoing;
+        Node<MP3Tags>* playingNow;  // Has a two-way link to the previous song() and the next song()
+                                    // Only this node is not a reference to the origin node, but a copy of the data, in case the origin list changes
+        bool conditioned;
+
         // Creates a CircularList class build upon a DoubleLinkedList
         // Parameters:
         // origin_ptr: is pointer to the memory address where the Linked List exists
@@ -75,7 +147,7 @@ class CircularList<MP3Tags> : public Observer{
             boost::uuids::random_generator gen;
             boost::uuids::uuid uuid = gen();
             this->identifier = boost::uuids::to_string(uuid);
-            this->ongoing = false;
+            this->conditioned = false;
             this->playingNow = nullptr;
             this->start = "";
         }
@@ -102,53 +174,6 @@ class CircularList<MP3Tags> : public Observer{
         int size(){
             return this->origin->size;
         }
-        // Changes the node order on the pointed origin list
-        void updateOrder(){
-            if (!ongoing){ // When the mp3 reproduction hasn't started
-                Node<MP3Tags>* ref = this->origin->GetNode(0); // A reference to the first node of the DoubleLinkedList
-                // Before changing any values must deallocate the pointers from memory
-                if (this->playingNow != nullptr){
-                    delete this->playingNow->next;
-                    delete this->playingNow->prev;
-                }
-                delete this->playingNow;
-                this->playingNow = nullptr;
-                // Now its viable to change any data
-                if (ref != nullptr){
-                    this->playingNow = new Node<MP3Tags>(ref->data); // Create a copy node of the contents
-                    if (ref->next != nullptr){
-                        this->playingNow->next = new Node<MP3Tags>(ref->next->data);
-                        this->playingNow->next->prev = this->playingNow;
-                    }
-                }
-                this->start = string(this->playingNow->data.uuid);
-            } else { // Once started
-                this->updateFollowing();
-            }
-        }
-        // Sets a the node with better voting difference as the following node
-        void updateFollowing(){
-            // Get the head node and iterate over the nodes to find the one with a better voting difference
-            // Set the better one AS the following node
-            Node<MP3Tags>* ref = this->origin->getHead();
-            Node<MP3Tags>* selected = nullptr;
-            int best_difference = 0;
-            if (ref != nullptr){
-                while(ref->next != nullptr){
-                    if ( (ref->data.upvotes - ref->data.downvotes) > best_difference && ref != this->playingNow){
-                        selected = ref;
-                    }
-                    ref = ref->next;
-                }
-            }
-            if (selected != nullptr){
-                delete this->playingNow->next;
-                this->playingNow->next = nullptr;
-                
-                this->playingNow->next = new Node<MP3Tags>(selected->data);
-                this->playingNow->next->prev = this->playingNow;
-            }
-        }
         // Forcefully modifies the current song for play and changes its pointers
         // Only usable for user decision
         // Expects a valid UUID represented as a string
@@ -170,21 +195,49 @@ class CircularList<MP3Tags> : public Observer{
             
             this->playingNow = new_node;
 
-            this->updateFollowing();
+            this->setFollowing();
         }
         // Gets an array of all artists in the playlist with their songs in the current playlist
-        // Returns and pointer to the list, and hence the pointer should be deallocated
+        // Returns and pointer to the list, and hence the pointer should be deallocated when it will be no longer used
         string* retrieveArtists(){
             return nullptr;
         }
-        // Returns the path the next song to reproduce
+        // Moves to the next song(pointer)
         // Implies a change of context for the currently played
-        const char* moveNextSong(){
-            return nullptr;
+        // If <reached_end> is set to true then the moved song is added to the already played list of songs
+        void moveNextSong(){
+            delete this->playingNow->prev;
+                this->playingNow->prev = nullptr;
+
+            this->alreadyPlayed.insert(string(this->playingNow->data.uuid));
+
+            if (this->origin->size == this->alreadyPlayed.size){ // What happens when the playlist has reached its end
+                // Reset all the played song ids
+                while (this->alreadyPlayed.size > 0){
+                    this->alreadyPlayed.pop();
+                }
+                // Set back the start of the list at the first node
+                this->conditioned = false;
+                this->updateOrder();
+                this->setFollowing();
+                this->conditioned = true;
+            }else {
+                this->playingNow = this->playingNow->next;
+                this->setFollowing();
+            }
         }
-        // Returns the path to the previous song to reproduce
-        const char* movePrevSong(){
-            return nullptr;
+        // Moves back to the previous song(pointer)
+        // Implies a change of context for the currently played
+        // If <reached_end> is set to true then the moved song is added to the already played list of songs
+        void movePrevSong(){
+            if (this->playingNow->prev == nullptr){
+                // Throw exception
+            }
+            delete this->playingNow->next;
+                this->playingNow->next = nullptr;
+
+            this->playingNow = this->playingNow->prev;
+            this->setPrevious();
         }
 };
 
