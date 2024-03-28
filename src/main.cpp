e1c47a91045996cb0289c188beb7592a9b9c38e7
fredeos <fobando@estudiantec.cr>
2024-03-27 23:37:59 -0600

    Moar changes to CircularList
    Added features for updating the currently played song forcelly and other for updating accordingly while the DoubleLinkedList is being build.

--- src/main.cpp
+++ src/main.cpp
@@ -15,46 +15,5 @@ using namespace std;
 namespace fs = boost::filesystem;
 // int main(int argc, char *argv[]){
 int main(){ // Code testing
-    // ------- Create the loader
-    Loader loader(string("/home/frederick/Desktop/bib/.cmp"));
 
-    // ------ Create a list of songs
-        // Filenames retrieval
-    string songs_path = "/home/frederick/Desktop/bib/playlist1";
-    vector< string > filenames;
-    try {
-        fs::directory_iterator end_iter;
-        for (fs::directory_iterator iter(songs_path); iter != end_iter; ++iter) {
-            if (fs::is_regular_file(*iter)) {
-                std::cout << "File: " << iter->path().filename() << std::endl;
-                filenames.push_back(iter->path().filename().string());
-            }
-            // You can add more conditions for other types of entries if needed
-        }
-    } catch (const std::exception& ex) {
-        std::cerr << "Error: " << ex.what() << std::endl;
-    }
-        // List creation
-    DoubleLinkedList<MP3Tags> songs;
-        songs.InsertAtEnd(MP3Tags(songs_path + "/" + filenames[0]));
-        songs.InsertAtEnd(MP3Tags(songs_path + "/" + filenames[1]));
-        songs.InsertAtEnd(MP3Tags(songs_path + "/" + filenames[2]));
-        songs.InsertAtEnd(MP3Tags(songs_path + "/" + filenames[3]));
-    
-    // Convert one of the songs to a wav
-    loader.Convert(string(songs.GetNode(0)->data.file), string("current"));
-
-    // Load and play the song
-    sf::Music player;
-    if (!player.openFromFile("/home/frederick/Desktop/bib/.cmp/current.wav")){
-        std::cout << "Player failed!" << std::endl;
-        return -1;
-    }
-
-    std::cout << "Playing" << std::endl;
-    player.play();
-    while (player.getStatus() == sf::Music::Playing){}
-
-    std::cout << "Song finished" << std::endl;
-    return 0;
 }
--- src/modules/c++/CircularList.h
+++ src/modules/c++/CircularList.h
@@ -65,6 +65,7 @@ class CircularList<MP3Tags> : public Observer{
         LinkedList<string> alreadyPlayed; // List of songs(uuids) that the have already been played and wont be played again(if the origin list changes), unless it is forced
         string start;  // Reference to the id of the song that was played first, and by consequence should play again when the playlist ends
     public:
+        bool ongoing;
         // Creates a CircularList class build upon a DoubleLinkedList
         // Parameters:
         // origin_ptr: is pointer to the memory address where the Linked List exists
@@ -74,6 +75,9 @@ class CircularList<MP3Tags> : public Observer{
             boost::uuids::random_generator gen;
             boost::uuids::uuid uuid = gen();
             this->identifier = boost::uuids::to_string(uuid);
+            this->ongoing = false;
+            this->playingNow = nullptr;
+            this->start = "";
         }
         // Destroys the circular list
         ~CircularList(){
@@ -88,6 +92,7 @@ class CircularList<MP3Tags> : public Observer{
             switch (state){
                 case type1:
                     std::cout << "Observer " << this->identifier <<" => " <<"List contents were modified" << std::endl;
+                    this->updateOrder();
                     break;
                 default:
                     break;
@@ -99,20 +104,87 @@ class CircularList<MP3Tags> : public Observer{
         }
         // Changes the node order on the pointed origin list
         void updateOrder(){
-
+            if (!ongoing){ // When the mp3 reproduction hasn't started
+                Node<MP3Tags>* ref = this->origin->GetNode(0); // A reference to the first node of the DoubleLinkedList
+                // Before changing any values must deallocate the pointers from memory
+                if (this->playingNow != nullptr){
+                    delete this->playingNow->next;
+                    delete this->playingNow->prev;
+                }
+                delete this->playingNow;
+                this->playingNow = nullptr;
+                // Now its viable to change any data
+                if (ref != nullptr){
+                    this->playingNow = new Node<MP3Tags>(ref->data); // Create a copy node of the contents
+                    if (ref->next != nullptr){
+                        this->playingNow->next = new Node<MP3Tags>(ref->next->data);
+                        this->playingNow->next->prev = this->playingNow;
+                    }
+                }
+                this->start = string(this->playingNow->data.uuid);
+            } else { // Once started
+                this->updateFollowing();
+            }
         }
-        // Gets a list of all artists in the playlist with the uuid of the song they located at
-        tuple<string, string>* retrieveArtists(){
+        // Sets a the node with better voting difference as the following node
+        void updateFollowing(){
+            // Get the head node and iterate over the nodes to find the one with a better voting difference
+            // Set the better one AS the following node
+            Node<MP3Tags>* ref = this->origin->getHead();
+            Node<MP3Tags>* selected = nullptr;
+            int best_difference = 0;
+            if (ref != nullptr){
+                while(ref->next != nullptr){
+                    if ( (ref->data.upvotes - ref->data.downvotes) > best_difference && ref != this->playingNow){
+                        selected = ref;
+                    }
+                    ref = ref->next;
+                }
+            }
+            if (selected != nullptr){
+                delete this->playingNow->next;
+                this->playingNow->next = nullptr;
+                
+                this->playingNow->next = new Node<MP3Tags>(selected->data);
+                this->playingNow->next->prev = this->playingNow;
+            }
+        }
+        // Forcefully modifies the current song for play and changes its pointers
+        // Only usable for user decision
+        // Expects a valid UUID represented as a string
+        void ForceSetCurrent(string song_id){
+            delete this->playingNow->next;
+                this->playingNow->next = nullptr;
+            delete this->playingNow->prev;
+                this->playingNow->prev = nullptr;
+            Node<MP3Tags>* current = this->origin->getHead();
+            while (current != nullptr){
+                if (string(current->data.uuid) == song_id){
+                    break;
+                }
+                current = current->next;
+            }
+            Node<MP3Tags>* new_node = new Node<MP3Tags>(current->data);
+                this->playingNow->next = new_node;
+                new_node->prev = this->playingNow;
+            
+            this->playingNow = new_node;
 
+            this->updateFollowing();
+        }
+        // Gets an array of all artists in the playlist with their songs in the current playlist
+        // Returns and pointer to the list, and hence the pointer should be deallocated
+        string* retrieveArtists(){
+            return nullptr;
         }
         // Returns the path the next song to reproduce
         // Implies a change of context for the currently played
         const char* moveNextSong(){
-
+            return nullptr;
         }
         // Returns the path to the previous song to reproduce
         const char* movePrevSong(){
-
+            return nullptr;
         }
 };
 
