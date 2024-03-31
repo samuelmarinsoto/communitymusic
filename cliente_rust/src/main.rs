a813873642752ee0738d06b7de25eb8ae0914bdd
emarojass05 <e84671818@gmail.com>
2024-03-30 22:17:14 -0600

    avance interfaz en dos librerias

--- cliente_rust/src/main.rs
+++ cliente_rust/src/main.rs
@@ -1,75 +1,55 @@
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow};
+use gtk::{Application, ApplicationWindow, Button, Label};
+use glib::signal::Inhibit; // Import Inhibit
 
 const APP_ID: &str = "org.gtk_rs.HelloWorld2";
 
-fn main() -> glib::ExitCode {
+fn main() {
     // Create a new application
-    let app = Application::builder().application_id(APP_ID).build();
+    let app = Application::builder()
+        .application_id(APP_ID)
+        .build();
 
     // Connect to "activate" signal of `app`
     app.connect_activate(build_ui);
 
     // Run the application
-    app.run()
+    app.run();
 }
 
 fn build_ui(app: &Application) {
+    // Create the application window
     let window = ApplicationWindow::builder()
         .application(app)
-        .title("Community Playlist")
-        .default_width(400) 
-        .default_height(300) 
         .build();
-    let button = gtk::Button::with_label("Click me!"); 
-    button.connect_clicked(|_| {
-        println!("Button clicked!");
+
+    window.set_app_paintable(true);
+    window.connect_draw(move |window, ctx| {
+        ctx.set_source_rgba(220.0/255.0, 239.0/255.0, 250.0/255.0, 1.0);
+        let _ = ctx.paint();
+        false.into()
     });
-    window.set_child(Some(&button));
-    window.present();
+
+
+    // Create a vertical box layout to hold buttons and labels
+    let vbox = gtk::Box::new(gtk::Orientation::Vertical, 5);
+    window.set_child(Some(&vbox));
+
+    // Create labels and buttons for each song
+    for i in 1..=10 {
+        let button = Button::new();
+        button.set_label(&format!("Song {}", i));
+        vbox.pack_start(&button, false, false, 0);
+
+        let label = Label::new(None);
+        label.set_text(&format!("c{}", i));
+        vbox.pack_start(&label, false, false, 0);
+
+        // Set size request for button to make it smaller
+        button.set_size_request(100, 30);
+        label.set_size_request(100, -1); // Set width same as button, height auto
+    }
+
+    // Show all widgets
+    window.show_all();
 }
-
-
-
-
-// use gtk::prelude::*;
-// use gtk::{Application, ApplicationWindow, Button};
-
-// const APP_ID: &str = "org.gtk_rs.HelloWorld2";
-
-// fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     // Crea una nueva aplicación
-//     let app = Application::builder().application_id(APP_ID).build();
-
-//     // Conecta la señal "activate" de la aplicación a la función build_ui
-//     app.connect_activate(build_ui);
-
-//     // Ejecuta la aplicación
-//     app.run();
-
-//     Ok(())
-// }
-
-// fn build_ui(app: &Application) {
-//     // Crea una ventana con la aplicación
-//     let window = ApplicationWindow::builder()
-//         .application(app)
-//         .title("Community Playlist")
-//         .default_width(400) // Establece el ancho de la ventana
-//         .default_height(300) // Establece la altura de la ventana
-//         .build();
-
-//     // Crea un botón
-//     let button = Button::with_label("Click me!");
-
-//     // Conecta la señal "clicked" del botón a una función
-//     button.connect_clicked(|_| {
-//         println!("Button clicked!");
-//     });
-
-//     // Agrega el botón a la ventana
-//     window.set_child(Some(&button));
-
-//     // Muestra la ventana
-//     window.present();
-// }
--- rust-tk/src/main.rs
+++ rust-tk/src/main.rs
@@ -1,16 +1,96 @@
 use rstk::*;
 
 fn main() {
-  let root = rstk::start_with("wish8.6").unwrap();
+    let root = rstk::start_with("wish8.6").unwrap();
 
-  // Set the window size to 800x600
-  root.geometry(800,600,0,0);
+    // Set the window size 
+    root.geometry(800,600,0,0);
 
-  let hello = rstk::make_label(&root);
-  hello.text("Hello from Rust/Tk");
-  hello.grid().layout();
-  let button = rstk::make_button(&root);
-  button.text("Click me");
-  button.grid().layout();
-  rstk::mainloop();
+    // // Set the window background color to purple
+    // root.configure("background", "#2596BE");
+    
+    let hello = rstk::make_label(&root);
+    hello.text("Comunity Playlist");
+    hello.grid().layout();
+
+    // Create buttons and labels for each song
+    let button1 = rstk::make_button(&root);
+    button1.text("Song 1");  // Emoji
+    button1.grid().row(5).column(5).layout();  
+
+    let label1 = rstk::make_label(&root);
+    label1.text("c1");
+    label1.grid().row(5).column(6).layout();
+
+    let button2 = rstk::make_button(&root);
+    button2.text("Song 2");
+    button2.grid().row(7).column(5).layout();  
+
+    let label2 = rstk::make_label(&root);
+    label2.text("c2");
+    label2.grid().row(7).column(6).layout();  
+
+    let button3 = rstk::make_button(&root);
+    button3.text("Song 3");
+    button3.grid().row(9).column(5).layout();  
+
+    let label3 = rstk::make_label(&root);
+    label3.text("c3");
+    label3.grid().row(9).column(6).layout();  
+
+    let button4 = rstk::make_button(&root);
+    button4.text("Song 4");
+    button4.grid().row(11).column(5).layout();  
+
+    let label4 = rstk::make_label(&root);
+    label4.text("c4");
+    label4.grid().row(11).column(6).layout();  
+
+    let button5 = rstk::make_button(&root);
+    button5.text("Song 5");
+    button5.grid().row(13).column(5).layout();  
+
+    let label5 = rstk::make_label(&root);
+    label5.text("c5");
+    label5.grid().row(13).column(6).layout();  
+
+    let button6 = rstk::make_button(&root);
+    button6.text("Song 6");
+    button6.grid().row(15).column(5).layout();  
+
+    let label6 = rstk::make_label(&root);
+    label6.text("c6");
+    label6.grid().row(15).column(6).layout();  
+
+    let button7 = rstk::make_button(&root);
+    button7.text("Song 7");
+    button7.grid().row(17).column(5).layout();  
+
+    let label7 = rstk::make_label(&root);
+    label7.text("c7");
+    label7.grid().row(17).column(6).layout();  
+
+    let button8 = rstk::make_button(&root);
+    button8.text("Song 8");
+    button8.grid().row(19).column(5).layout();  
+    let label8 = rstk::make_label(&root);
+    label8.text("c8");
+    label8.grid().row(19).column(6).layout();  
+
+    let button9 = rstk::make_button(&root);
+    button9.text("Song 9");
+    button9.grid().row(21).column(5).layout(); 
+    let label9 = rstk::make_label(&root);
+    label9.text("c9");
+    label9.grid().row(21).column(6).layout();  
+
+    let button10 = rstk::make_button(&root);
+    button10.text("Song 10");
+    button10.grid().row(23).column(5).layout();  
+
+    let label10 = rstk::make_label(&root);
+    label10.text("c10");
+    label10.grid().row(23).column(6).layout();  
+
+    rstk::mainloop();
 }
